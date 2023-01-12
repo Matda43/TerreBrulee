bfac3e47a7d53b1128e994a456c513f090b50729
Matda43 <matda43@gmail.com>
2023-01-12 09:53:35 +0100

    adding moon rotation movement

--- TerreBrulee/Assets/Scripts/Moon.cs
+++ TerreBrulee/Assets/Scripts/Moon.cs
@@ -7,47 +7,16 @@ public class Moon : MonoBehaviour
     // Start is called before the first frame update
 
     const float DISTANCE_FROM_EARTH = 3;
-
-    Location spawn_location = new Location(0, DISTANCE_FROM_EARTH);
-
-    float angle = 10;
+    Vector2 spawn_location = new Vector2(DISTANCE_FROM_EARTH, 0);
 
     void Start()
     {
-        setPosition(spawn_location);
+        this.transform.position = spawn_location;
     }
 
     // Update is called once per frame
     void Update()
     {
-        move2();
-    }
 
-    private void move()
-    {
-        float posX = Mathf.Cos(angle);
-        float posY = Mathf.Sin(angle);
-        this.transform.position = new Vector2(posX, posY) * DISTANCE_FROM_EARTH;
-        angle = angle + Time.deltaTime;
-        angle = angle % 360f;
-    }
-
-    private void move2()
-    {
-        float rad = Mathf.Atan2(Input.mousePosition.y, Input.mousePosition.x);
-
-        float deg = Mathf.Round(rad * (180 / Mathf.PI));
-
-        //angle = Mathf.Round(rad * (180 / Mathf.PI));
-        float posX = Mathf.Cos(deg);
-        float posY = Mathf.Sin(deg);
-        
-        this.transform.position = new Vector2(posX, posY) * DISTANCE_FROM_EARTH;
-        //angle = angle % 360f;
-    }
-
-    private void setPosition(Location new_Location)
-    {
-        this.transform.position = new Vector2(new_Location.getX(), new_Location.getY());
     }
 }
