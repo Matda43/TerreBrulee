baf42fe732fb3d6c468bcef82b496cfdcb765a81
Savro01 <romain.savaete@outlook.com>
2023-01-17 10:10:00 +0100

    Collision detection and fonctionnal game

--- TerreBrulee/Assets/Scripts/Earth.cs
+++ TerreBrulee/Assets/Scripts/Earth.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moon : MonoBehaviour
+public class Earth : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    const float DISTANCE_FROM_EARTH = 2.25f;
-    Vector2 spawn_location = new Vector2(DISTANCE_FROM_EARTH, 0);
-
     void Start()
     {
-        this.transform.position = spawn_location;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
+        openEndMessage();
+        openMenu();
+        Time.timeScale = 0;
     }
 
+    void openEndMessage()
+    {
+
+    }
+
+    void openMenu()
+    {
+
+    }
 }
